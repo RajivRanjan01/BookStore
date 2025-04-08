391127b4ce869e0d889b2e6dadedd6da1a16016c
RajivRanjan01 <rajivranjan12142@gmail.com>
2025-04-09 04:36:10 +0530

    Implement user login with JWT authentication
    This commit adds the functionality for users to log in and receive a JWT.
    -  Added LoginRequest and JwtResponse DTOs.
    -  Updated AuthController to handle login and generate JWTs.
    -  Created JwtUtil to generate and validate JWTs.
    -  Added JWT properties to application.properties.

---
+++ src/main/java/com/example/Bookstore/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.example.Bookstore.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")  //  Read the secret key from application.properties
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")  //  Read the expiration time from application.properties
+    private int jwtExpirationMs;
+
+    public String generateToken(Authentication authentication) {
+
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+
+        }
+
+        return false;
+    }
+}
